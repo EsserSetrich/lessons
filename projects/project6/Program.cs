def043caf65b5097a2edd2774d0529a994005ef1
Setrich <setrich@mail.ru>
2022-11-16 22:11:58 +0300

    Д/з показывает все чётные числа от 1 до N.

---
+++ projects/project6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+ Console.Write("Введите первое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while( count < (num + 1))
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+    count += 1;
+}
