03088083c0707bb1b6ead64ba21fae9a9e2dcea0
Setrich <setrich@mail.ru>
2022-11-19 11:42:55 +0300

    Д/з показывает вторую цифру трёхзначного  числа.

---
+++ projects/project7/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите первое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+//int num = new Random(). Next(100, 1000);
+//Console.WriteLine(num);
+int num2 = num/10;
+//Console.WriteLine(num2);
+int num3 = num2%10;
+Console.WriteLine(num3);
