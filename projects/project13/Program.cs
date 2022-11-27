bd6ccd132f17645588505554f0a7604e9a3a3499
Setrich <setrich@mail.ru>
2022-11-27 12:53:03 +0300

    Д/З возводит число A в натуральную степень B.

---
+++ projects/project13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int num = ReadInt("Введите первое число: ");
+int num2 = ReadInt("Введите второе число: ");
+
+    int result = 1;
+    for (int i = 1; i <= num2; i++)
+    {
+        result = result * num;
+    }
+    Console.WriteLine(result);
+
+// // Функция ввода
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
